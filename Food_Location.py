d6cc6594174814f71c28d302876ed2fd14d61801
Lesze <lesterandrew.arollado.cvt@eac.edu.ph>
2025-04-18 21:42:22 +0800

    initial commit

---
+++ Food_Location.py
@@ -0,0 +1,14 @@
+#HELLO THERE WELCOME TO YOUR FOOD HAVEN! Looking For good places to eat out? here is options depending on your location
+location = input("Where are you?(CAVITE OR MANILA): ")
+if location == "CAVITE":
+    food = input("what food do you want?(CHICKEN OR PORK): ")
+    if food == "CHICKEN":
+        print("WINGS OR CHICKEN OPTIONS:\nDOC WINGS\nTOSS WINGS\n3 JOINS")
+    elif food == "PORK":
+         print("PORK OR SAMG OPTIONS:\nSAMGYUPSALAMAT\nYAKIMIX")
+elif location == "MANILA":
+    food = input("what food do you want?(CHICKEN OR PORK): ")
+    if food == "CHICKEN":
+        print("WINGS OR CHICKEN OPTIONS:\nDOC WINGS\nTOSS WINGS\n3 JOINS")
+    elif food == "PORK":
+         print("PORK OR SAMG OPTIONS:\nSAMGYUPSALAMAT\nYAKIMIX")
